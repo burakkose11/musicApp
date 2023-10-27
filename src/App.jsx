631bd13b3c142bcee1a68792acefc57f4cdd1a6e
Burak Köse <burakkose011@icloud.com>
2023-10-27 21:27:35 +0300

    App.jsx style eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {FlatList, SafeAreaView, Text} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, Text} from 'react-native';
 import music_data from './Data/music-data.json';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <FlatList
         data={music_data}
         renderItem={({item}) => <Text>{item.artist}</Text>}
@@ -12,5 +12,12 @@ const App = () => {
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#eceff1',
+    flex: 1,
+  },
+});
 
 export default App;
